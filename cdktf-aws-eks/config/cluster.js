e88af92370c1f268b86fe7497e1cf228448cdf8a
Olivier PILLAUD TIRARD <olivier.pillaud-tirard@manomano.com>
2022-02-08 14:53:54 +0100

    add some experiment

---
+++ cdktf-aws-eks/config/cluster.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.clusterConfig = void 0;
+exports.clusterConfig = {
+    applications: ['nginx', '2048']
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2x1c3Rlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImNsdXN0ZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQWEsUUFBQSxhQUFhLEdBQUc7SUFDekIsWUFBWSxFQUFFLENBQUMsT0FBTyxFQUFFLE1BQU0sQ0FBQztDQUNsQyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IGNsdXN0ZXJDb25maWcgPSB7XG4gICAgYXBwbGljYXRpb25zOiBbJ25naW54JywgJzIwNDgnXVxufSJdfQ==
